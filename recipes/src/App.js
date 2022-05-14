b56bbb93ae6709f38896885e85f72716d019cefb
Min Choi <root@DESKTOP-MIN-NCASEM1>
2022-05-14 16:26:09 -0700

    Updated files test

---
+++ recipes/src/App.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>10 Minute Recipes</h1>
+      <h2>TEST TEST TEST TEST TEST</h2>
+      <h3>Min</h3>
+    </div>
+  );
+}
