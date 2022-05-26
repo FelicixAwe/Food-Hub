d27b6b9ac874a3b1c9a46f3f435ff69e19348a38
Min Choi <root@DESKTOP-MIN-NCASEM1>
2022-05-25 18:22:47 -0700

    Revert "Trying to add a Iframe to each tile clicked, so it can render within the app"
    This reverts commit cdbc100520817561ff8bab83d7ccf79b29e9fa38.

--- recipes/src/recipetile/Recipetile.js
+++ recipes/src/recipetile/Recipetile.js
@@ -1,20 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './style.css';
 import Likebutton from './Likebutton.js';
-
-function openIframe (recipe) {
-  return (
-    <iframe src={recipe["recipe"]["url"]} frameborder="0" width="500" height="500"></iframe>
-  )
-  /*return (
-    <iframe src={recipe["recipe"]["url"]} 
-            width={1000} 
-            height={500} 
-            title={recipe["recipe"]["label"]}>
-    </iframe>
-  )*/ 
-}
-
 function Recipetile({recipe}){
   return(
     // recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
@@ -23,9 +9,7 @@ function Recipetile({recipe}){
           className="recipeTile__image"
           src={recipe["recipe"]["image"]}
           alt="tile-image"
-          //onClick={() => window.open(recipe["recipe"]["url"])}
-          onClick={() =>  openIframe()
-            }
+          onClick={() => window.open(recipe["recipe"]["url"])}
         />
         <p className="recipeTile__name">{recipe["recipe"]["label"]}</p>
           <Likebutton/>
